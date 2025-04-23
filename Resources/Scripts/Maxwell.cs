32f22f23442eab85d95c339eee572659c64df568
FantomTchi7 <vladislav.kudriashev@gmail.com>
2025-04-23 22:57:42 +0300

    Initial commit

---
+++ Resources/Scripts/Maxwell.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Maxwell : Node3D
+{
+	private float RotationSpeedRadians = Mathf.DegToRad(200f);
+	
+	public override void _Process(double delta)
+	{
+		float radiansToRotate = -RotationSpeedRadians * (float)delta;
+
+		RotateY(radiansToRotate);
+
+		GD.Print(Rotation.Y);
+	}
+}
